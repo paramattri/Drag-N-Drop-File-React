a6dc71adf63299523f785c4f6efe664953de075b
Param Attri <attri.param@gmail.com>
2021-01-08 11:18:35 +0530

    first commit

---
+++ drag-n-drop-react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import DragNDrop from './DragNDrop';
+
+function App() {
+  return (
+    <div className="app">
+      <h1 style={{textAlign: 'center'}}>Drag N Drop Admin Page</h1>
+      <DragNDrop/>
+    </div>
+  );
+}
+
+export default App;
---
+++ drag-n-drop-react/src/DragNDrop.js
@@ -0,0 +1,62 @@
+import React, {useState, useEffect} from 'react'
+import {useDropzone} from 'react-dropzone';
+import axios from 'axios'
+
+function DragNDrop() {
+
+    const { getRootProps, getInputProps } = useDropzone();
+    const [config, setConfig] = useState()
+
+    ondragover = (e) => {
+        e.preventDefault()
+    }
+
+    ondrop = (e) => {
+        e.preventDefault()
+        //console.log(e.dataTransfer.files)
+        Array.from(e.dataTransfer.files)
+        .filter((file) => file.type === "application/json" && file.name === "insurance_config.json")
+        .forEach(async (file) => {
+            const text = await file.text()
+            console.log(text)
+            setConfig(text)
+        })
+    }
+
+    const sendConfigData = () => {
+        (
+            typeof config !== 'undefined'
+             &&
+            axios.post("https://reqres.in/api/users", config).then((response) => {
+                console.log("Config Data Sent")
+                console.log(response)
+            })
+        )
+    }
+
+    useEffect(() => {
+        (
+            typeof config !== 'undefined'
+             &&
+            console.log("Changes in File Detected")
+            // axios.post("https://reqres.in/api/users", config).then((response) => {
+            //     console.log("Config Data Sent")
+            //     console.log(response)
+            // })
+        )
+    }, [config])
+
+    return (
+        <div className="dragDrop">
+            <div {...getRootProps({className: 'dropzone'})} style={{padding: 30, border: '2px dotted gray', width: '60%', margin: 'auto', marginTop: 50}}>
+                <input {...getInputProps()}/>
+                <p>Drag 'n' drop some files here, or click to select files</p>
+            </div>
+            <p>{config}</p>
+            <button onClick={sendConfigData}>Send Data</button>
+        </div>
+        
+    )
+}
+
+export default DragNDrop

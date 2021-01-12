35aa06d43cabc225a20cae56c8f86476c146093d
Param Attri <attri.param@gmail.com>
2021-01-12 13:10:16 +0530

    random color generation added

--- drag-n-drop-react/src/charts/BarChart.js
+++ drag-n-drop-react/src/charts/BarChart.js
@@ -3,14 +3,32 @@ import { Bar } from 'react-chartjs-2'
 
 function BarChart() {
 
+    var borderColor = []
+    var backgroundColor = []
+
+    const fillColor = () => {
+
+        for(var i = 0; i < 4; i++){
+            borderColor.push('rgba(255, 206, 86, 0.2)')
+            backgroundColor.push('rgba(255, 206, 86, 0.2)')
+        }
+
+        // borderColor = Array(4).fill('rgba(255, 206, 86, 0.2)')
+        // backgroundColor = Array(4).fill('rgba(255, 206, 86, 0.2)')
+        console.log(borderColor)
+        console.log(backgroundColor)
+    }
+
     const data = {
         labels: ["Motor Insurance", "Travel Insurance", "Health Insurance", "Term Life Insurance"],
         datasets: [
             {
                 label: "Partners for Categories",
                 data: [2,2,2,3],
-                borderColor: ['rgba(255, 206, 86, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(255, 206, 86, 0.2)'],
-                backgroundColor: ['rgba(255, 206, 86, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(255, 206, 86, 0.2)']
+                // borderColor: ['rgba(255, 206, 86, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(255, 206, 86, 0.2)'],
+                // backgroundColor: ['rgba(255, 206, 86, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(255, 206, 86, 0.2)']
+                borderColor: borderColor,
+                backgroundColor: backgroundColor
             }
         ]
     }
@@ -35,6 +53,7 @@ function BarChart() {
 
     return (
         <div style={{width: "80%", margin: 'auto'}}>
+            {fillColor()}
             <Bar data={data} options={options}/>
         </div>
     )
--- drag-n-drop-react/src/charts/DoughnutChart1.js
+++ drag-n-drop-react/src/charts/DoughnutChart1.js
@@ -3,18 +3,27 @@ import { Doughnut } from 'react-chartjs-2'
 
 function DoughnutChart1() {
 
+    var backgroundColor = []
+    var rgb = []
+
+    const randomColorGenerate = () => {
+        for(var i = 0; i < 4; i++){
+            for(var j = 0; j < 3; j++){
+                rgb.push(Math.floor(Math.random() * 255))
+            }
+            backgroundColor.push('rgb('+rgb.join(',')+')')
+            rgb = []
+        }
+        console.log(backgroundColor)
+    }
+
     const data = {
         labels: ["Tata AIG", "Bajaj Allianz", "HDFC ERGO", "HDFC Life"],
         datasets: [
             {
               label: 'Partners',
               data: [3, 3, 1, 1],
-              backgroundColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(255, 205, 86, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 159, 64, 1)'
-              ]
+              backgroundColor: backgroundColor
             }
           ]
     }
@@ -28,6 +37,7 @@ function DoughnutChart1() {
 
     return (
         <div style={{width: "40%"}}>
+            {randomColorGenerate()}
             <Doughnut data={data} options={options}/> 
         </div>
     )
